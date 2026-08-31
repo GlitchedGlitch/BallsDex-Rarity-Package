f7e1916d2b0bb6b6844c9f56dcd4bf09ba1b4b24
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-08-31 23:53:35 +0200

    Update apps.py

--- rarity/apps.py
+++ rarity/apps.py
@@ -6,3 +6,21 @@ class RarityConfig(AppConfig):
     verbose_name = "Rarity"
     default_auto_field = "django.db.models.BigAutoField"
     dpy_package = "rarity.rarity"
+
+    def ready(self):
+        from settings.models import Settings
+        from rarity.models import RaritySettings
+        
+        try:
+            global_settings = Settings.objects.first()
+            if global_settings:
+                RaritySettings.objects.get_or_create(
+                    settings=global_settings,
+                    defaults={
+                        "embed_color": "",
+                        "style": RaritySettings.Style.CONTAINER,
+                        "buttons_inside": True,
+                    }
+                )
+        except Exception:
+            pass
