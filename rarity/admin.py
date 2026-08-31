adcc6ceb784ee743f307087546515ec1a51ad67c
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-08-31 21:54:14 +0200

    Fix migrations issues

--- rarity/admin.py
+++ rarity/admin.py
@@ -1,36 +1,38 @@
 from django.contrib import admin
-from settings.admin import SettingsAdmin
 from settings.models import Settings
 
 
-# Extend the existing SettingsAdmin to include rarity fields
-class RaritySettingsAdmin(SettingsAdmin):
-    fieldsets = list(SettingsAdmin.fieldsets) if hasattr(SettingsAdmin, "fieldsets") else []
-    
-    # Add rarity settings as a new fieldset
-    rarity_fieldset = (
-        "Rarity Settings",
-        {
-            "fields": (
-                "rarity_embed_color",
-                "rarity_style",
-                "rarity_buttons_inside",
-            ),
-        },
-    )
-    
-    # Try to insert before the first fieldset, or append
-    fieldsets.insert(0, rarity_fieldset)
-    
-    # Also add to list_display if desired
-    if hasattr(SettingsAdmin, "list_display"):
-        list_display = list(SettingsAdmin.list_display)
-        if "rarity_style" not in list_display:
-            list_display.append("rarity_style")
-        # Re-assign
-        SettingsAdmin.list_display = tuple(list_display)
+_existing_admin = admin.site._registry.get(Settings)
 
 
-# Monkey-patch the admin class
-admin.site.unregister(Settings)
-admin.site.register(Settings, RaritySettingsAdmin)
+class RaritySettingsAdmin(_existing_admin.__class__ if _existing_admin else admin.ModelAdmin):
+    """
+    Extend the existing Settings admin to include rarity fields.
+    """
+    
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = list(super().get_fieldsets(request, obj)) if self.fieldsets else []
+        
+        rarity_fieldset = (
+            "Rarity Settings",
+            {
+                "fields": (
+                    "rarity_embed_color",
+                    "rarity_style",
+                    "rarity_buttons_inside",
+                ),
+            },
+        )
+        
+        # Insert after first fieldset if exists, otherwise at start
+        if fieldsets:
+            fieldsets.insert(1, rarity_fieldset)
+        else:
+            fieldsets = [rarity_fieldset]
+        
+        return fieldsets
+
+
+if _existing_admin:
+    admin.site.unregister(Settings)
+    admin.site.register(Settings, RaritySettingsAdmin)
