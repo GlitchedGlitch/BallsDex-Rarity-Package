a76e0a524a19b4b4ebc158845dc53a6987236b4b
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-08-31 22:07:51 +0200

    Fix migrations issues
    Refactor RaritySettings admin registration and fieldsets.

--- rarity/admin.py
+++ rarity/admin.py
@@ -1,38 +1,17 @@
 from django.contrib import admin
-from settings.models import Settings
+from .models import RaritySettings
 
 
-_existing_admin = admin.site._registry.get(Settings)
-
-
-class RaritySettingsAdmin(_existing_admin.__class__ if _existing_admin else admin.ModelAdmin):
-    """
-    Extend the existing Settings admin to include rarity fields.
-    """
-    
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = list(super().get_fieldsets(request, obj)) if self.fieldsets else []
-        
-        rarity_fieldset = (
-            "Rarity Settings",
-            {
-                "fields": (
-                    "rarity_embed_color",
-                    "rarity_style",
-                    "rarity_buttons_inside",
-                ),
-            },
-        )
-        
-        # Insert after first fieldset if exists, otherwise at start
-        if fieldsets:
-            fieldsets.insert(1, rarity_fieldset)
-        else:
-            fieldsets = [rarity_fieldset]
-        
-        return fieldsets
-
-
-if _existing_admin:
-    admin.site.unregister(Settings)
-    admin.site.register(Settings, RaritySettingsAdmin)
+@admin.register(RaritySettings)
+class RaritySettingsAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                "settings",
+                "embed_color",
+                "style",
+                "buttons_inside",
+            ),
+        }),
+    )
+    list_display = ("settings", "style", "buttons_inside")
