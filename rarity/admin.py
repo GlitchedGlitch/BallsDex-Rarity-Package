04a19a8903e4e457d8ac0e10d5afc2baed784f0c
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-08-31 23:53:10 +0200

    Update admin.py

--- rarity/admin.py
+++ rarity/admin.py
@@ -1,51 +1,41 @@
 from django.contrib import admin
-from settings.admin import SettingsAdmin
-from settings.models import Settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import RaritySettings
 
 
-class RaritySettingsInline(admin.StackedInline):
-    """
-    Inline admin for RaritySettings, shown inside the main Settings edit page.
-    """
-    model = RaritySettings
-    can_delete = False
-    verbose_name = "Rarity Settings"
-    verbose_name_plural = "Rarity Settings"
-    fields = ("embed_color", "style", "buttons_inside")
-    classes = ("collapse",)
-
-def _patched_get_inlines(self, request, obj=None):
-    inlines = []
-    for inline_class in type(self).inlines if hasattr(type(self), 'inlines') else []:
-        inlines.append(inline_class)
-    inlines.append(RaritySettingsInline)
-    return inlines
-
-if not hasattr(SettingsAdmin, '_rarity_patched'):
-    SettingsAdmin.get_inlines = _patched_get_inlines
-    SettingsAdmin._rarity_patched = True  # type: ignore
-
-_original_save_model = SettingsAdmin.save_model
-
-
-def _patched_save_model(self, request, obj, form, change):
-    _original_save_model(self, request, obj, form, change)
-    RaritySettings.objects.get_or_create(
-        settings=obj,
-        defaults={
-            "embed_color": "",
-            "style": RaritySettings.Style.CONTAINER,
-            "buttons_inside": True,
-        }
+@admin.register(RaritySettings)
+class RaritySettingsAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                "embed_color",
+                "style",
+                "buttons_inside",
+            ),
+            "description": "Configure how the rarity command displays its output.",
+        }),
     )
-
-
-if not hasattr(SettingsAdmin, '_rarity_save_patched'):
-    SettingsAdmin.save_model = _patched_save_model
-    SettingsAdmin._rarity_save_patched = True  # type: ignore
-
-try:
-    admin.site.unregister(RaritySettings)
-except admin.sites.NotRegistered:
-    pass
+    
+    def has_add_permission(self, request):
+        return not RaritySettings.objects.exists()
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if "settings" in form.base_fields:
+            del form.base_fields["settings"]
+        return form
+    
+    def changelist_view(self, request, extra_context=None):
+        try:
+            obj = RaritySettings.objects.select_related("settings").get()
+            return HttpResponseRedirect(
+                reverse("admin:settings_raritysettings_change", args=[obj.pk])
+            )
+        except (ObjectDoesNotExist, RaritySettings.MultipleObjectsReturned):
+            pass
+        return super().changelist_view(request, extra_context=extra_context)
