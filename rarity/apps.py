4ac54e4439d7fd6792a39486a462aef686aa083c
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-09-24 20:53:24 +0200

    wipe everything crine

--- rarity/apps.py
+++ rarity/apps.py
@@ -6,27 +6,3 @@ class RarityConfig(AppConfig):
     verbose_name = "Rarity"
     default_auto_field = "django.db.models.BigAutoField"
     dpy_package = "rarity.rarity"
-
-    def ready(self):
-        from settings.models import Settings
-        from rarity.models import RaritySettings
-        
-        try:
-            global_settings = Settings.objects.first()
-            if global_settings:
-                RaritySettings.objects.get_or_create(
-                    settings=global_settings,
-                    defaults={
-                        "embed_color": "",
-                        "style": RaritySettings.Style.CONTAINER,
-                        "buttons_inside": True,
-                        "tier_mode": False,
-                        "entries_per_page": 7,
-                        "search_enabled": True,
-                        "rarity_search_enabled": True,
-                        "ephemeral_enabled": True,
-                        "show_thumbnail": True,
-                    }
-                )
-        except Exception:
-            pass
