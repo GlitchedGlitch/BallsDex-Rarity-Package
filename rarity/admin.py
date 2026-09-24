9d705e92e4b4cd260656383de991896cd3013fcf
Glitch <126723902+GlitchedGlitch@users.noreply.github.com>
2026-09-24 20:24:21 +0200

    Add rarity tags model

--- rarity/admin.py
+++ rarity/admin.py
@@ -1,8 +1,21 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import RaritySettings
+from .models import RaritySettings, RarityTag
+
+
+class RarityTagInline(admin.TabularInline):
+    model = RarityTag
+    extra = 1
+    fields = ("rarity_value", "is_tier_tag", "tag_text")
+
+
+@admin.register(RarityTag)
+class RarityTagAdmin(admin.ModelAdmin):
+    list_display = ("rarity_value", "is_tier_tag", "tag_text")
+    list_filter = ("is_tier_tag",)
+    search_fields = ("tag_text",)
 
 
 @admin.register(RaritySettings)
@@ -35,11 +48,12 @@ class RaritySettingsAdmin(admin.ModelAdmin):
         ("Filtering", {
             "fields": (
                 "hidden_balls",
+                "hidden_specials",
             ),
-            "description": "Hide specific balls from the rarity list.",
-            "classes": ("collapse",),
+            "description": "Hide specific balls/specials from the rarity list.",
         }),
     )
+    inlines = [RarityTagInline]
     
     def has_add_permission(self, request):
         return not RaritySettings.objects.exists()
@@ -49,9 +63,26 @@ class RaritySettingsAdmin(admin.ModelAdmin):
     
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
+        # Remove settings field from form
         if "settings" in form.base_fields:
             del form.base_fields["settings"]
         return form
+    
+    def save_model(self, request, obj, form, change):
+        # Auto-assign settings if not set
+        if obj.settings_id is None:
+            from settings.models import Settings
+            global_settings = Settings.objects.first()
+            if global_settings:
+                obj.settings = global_settings
+            else:
+                self.message_user(
+                    request,
+                    "No global Settings instance found! Please create one first.",
+                    messages.ERROR,
+                )
+                return
+        super().save_model(request, obj, form, change)
     
     def changelist_view(self, request, extra_context=None):
         try:
